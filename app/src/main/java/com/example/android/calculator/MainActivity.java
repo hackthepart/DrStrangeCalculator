7e914c726b20934db4a9e3c68e0c3fbaab478726
kriti dwivedi <dwivedi.kriti2@gmail.com>
2017-12-23 23:09:57 +0530

    Revert "Added all features in TODO (except widget!)  "

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -1,59 +1,27 @@
 package com.example.android.calculator;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
-
-
-
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
-
-
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private ShakerMethod mShakeDetector;
 Button plus,minus,multiply,divide;
-public static  TextView input_et;
-public static TextView result_tv;
-    double res=0,a=0,previousAnswer=0;
-   // boolean set=false;
-public static char operation='0';
-    public static String expression;
-
-
-
+TextView input_et;
+TextView result_tv;
+double a=0,res=0;
+    boolean set=false;
+char operation='0';
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mShakeDetector = new ShakerMethod();
-        mShakeDetector.setOnShakeListener(new ShakerMethod.OnShakeListener() {
-
-            @Override
-            public void onShake(int count) {
-                handleShakeEvent(count);
-            }
-        });
-        expression="";
         setContentView(R.layout.activity_main);
         plus=(Button)findViewById(R.id.add);
         minus=(Button)findViewById(R.id.subtract);
@@ -61,11 +29,6 @@ public static char operation='0';
         divide=(Button)findViewById(R.id.divide);
         input_et=(TextView)findViewById(R.id.input_et);
         result_tv=(TextView)findViewById(R.id.result_tv);
-    }
-    public void handleShakeEvent(int count){
-        input_et.setText("0.0");
-        result_tv.setText("0.0");
-        expression="";
     }
 
     @Override
@@ -89,7 +52,7 @@ public static char operation='0';
 
         return super.onOptionsItemSelected(item);
     }
-    public void display(View view) throws ScriptException {
+    public void display(View view) {
         switch(operation){
             case '+':
                 res+=a;
@@ -103,212 +66,116 @@ public static char operation='0';
             case '/':
                 res/=a;
                 break;
-            case '^':
-                res=Math.pow(res,a);
             default:
                 break;
         }
-        try {
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
-            res = (double) engine.eval(expression);
-            a = res;
-            result_tv.setText("" + res);
-            input_et.setText("0.0");
-        }
-        catch (Exception e){
-            return;
-        }
+        a=res;
+        result_tv.setText(""+res);
     }
-
-    public void addToMemory(View view){
-        expression="";
-        previousAnswer=res;
-        result_tv.setText("Previous Answer "+previousAnswer);
-        res=0;
-        input_et.setText(Double.toString(res));
-    }
-    public void subtractFromMemory(View view){
-        expression+= previousAnswer+" ";
-        input_et.setText(expression);
-    }
-
-
-    public void power(View view){
-        if(!(expression.charAt(expression.length()-1)=='^')) {
-            res = a;
-            result_tv.setText("" + res);
-            a = 0;
-            operation = '^';
-            expression+=" "+operation+" ";
-
-            input_et.setText(" " + expression);
-        }
-
-    }
-    public void openBrace(View view){
-        if(!(expression.charAt(expression.length()-1)=='(')) {
-            res = a;
-            result_tv.setText("" + res);
-            //set=true;
-            a = 0;
-            expression+=" ( ";
-            input_et.setText(" " + expression);
-        }
-
-    }
-    public void closeBrace(View view){
-        if(!(expression.charAt(expression.length()-1)==')')) {
-            res = a;
-            result_tv.setText("" + res);
-            //set=true;
-            a = 0;
-            expression+=" ) ";
-            input_et.setText(" " + expression);
-        }
-
-    }
-
 
     public void clear(View view){
         res=0;
         a=0;
-        expression="";
-        //set=false;
+        set=false;
         operation= '0';
         result_tv.setText(""+res);
-        input_et.setText(" "+expression);
+        input_et.setText(""+a);
     }
     public void add(View view){
-        if(!(expression.charAt(expression.length()-1)=='+')) {
-            res = a;
-            result_tv.setText("" + res);
-            //set=true;
-
-            //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show();
-            //  a=Float.parseFloat(input_et.getText().toString());
-            a=0;
-            operation = '+';
-            expression+=" "+operation+" ";
-            input_et.setText(" " + expression);
+        if(!set){
+            res=a;
+            result_tv.setText(""+res);
+            set=true;
         }
+      //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show();
+      //  a=Float.parseFloat(input_et.getText().toString());
+        a=0;
+        operation='+';
+        input_et.setText(""+a);
     }
     public void subtract(View view){
-       if(!(expression.charAt(expression.length()-1)=='-')) {
-           res = a;
-           result_tv.setText("" + res);
-           //set=true;
-
-           //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show();
-           // a=Float.parseFloat(input_et.getText().toString());
-           a = 0;
-           operation = '-';
-           expression+=" "+operation+" ";
-           input_et.setText(" " + expression);
+       if(!set){
+           res=a;
+           result_tv.setText(""+res);
+           set=true;
        }
+        //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show();
+       // a=Float.parseFloat(input_et.getText().toString());
+        a=0;
+        operation='-';
+        input_et.setText(""+a);
     }
     public void multiply(View view){
-        if(!(expression.charAt(expression.length()-1)=='*')) {
-            res = a;
-            result_tv.setText("" + res);
-            //set=true;
-            //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show()
-            a = 0;
-            operation = '*';
-            expression+=" "+operation+" ";
-            input_et.setText(" " + expression);
+        if(!set){
+            res=a;
+            result_tv.setText(""+res);
+            set=true;
         }
+        //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show()
+        a=0;
+        operation='*';
+        input_et.setText(""+a);
     }
     public void divide(View view){
-        if(!(expression.charAt(expression.length()-1)=='/')) {
-            res = a;
-            result_tv.setText("" + res);
-            //set=true;
-            //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show();
-            //  a=Float.parseFloat(input_et.getText().toString());
-            a = 0;
-            operation = '/';
-            expression+=" "+operation+" ";
-
-            input_et.setText(" " + expression);
+        if(!set){
+            res=a;
+            result_tv.setText(""+res);
+            set=true;
         }
+        //  Toast.makeText(this,"Plus button clicked",Toast.LENGTH_SHORT).show();
+      //  a=Float.parseFloat(input_et.getText().toString());
+        a=0;
+        operation='/';
+        input_et.setText(""+a);
     }
     public void value1(View view){
-        //a*=10;
-        a=1;
-        expression+=(int)a;
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=1;
+        input_et.setText(""+a);
     }
     public void value2(View view){
-        //a*=10;
-        a=2;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=2;
+        input_et.setText(""+a);
     }
     public void value3(View view){
-        //a*=10;
-        a=3;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=3;
+        input_et.setText(""+a);
     }
     public void value4(View view){
-        //a*=10;
-        a=4;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=4;
+        input_et.setText(""+a);
     }
     public void value5(View view){
-        //a*=10;
-        a=5;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=5;
+        input_et.setText(""+a);
     }
     public void value6(View view){
-        //a*=10;
-        a=6;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=6;
+        input_et.setText(""+a);
     }
     public void value7(View view){
-        //a*=10;
-        a=7;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=7;
+        input_et.setText(""+a);
     }
     public void value8(View view){
-        //a*=10;
-        a=8;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=8;
+        input_et.setText(""+a);
     }
     public void value9(View view){
-        //a*=10;
-        a=9;
-        expression+=(int)a;
-
-        input_et.setText(" "+expression);
+        a*=10;
+        a+=9;
+        input_et.setText(""+a);
     }
     public void value0(View view){
-        //a*=10;
-        expression+='0';
-        input_et.setText(" "+expression);
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
-    }
-
-    @Override
-    public void onPause() {
-        mSensorManager.unregisterListener(mShakeDetector);
-        super.onPause();
+        a*=10;
+        input_et.setText(""+a);
     }
 
 
