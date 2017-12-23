a8b580957153e8072c27269f443c3396228cd2d7
UTSAV JAIN@LAPTOP-AMVGD54G <utsavslife@gmail.com>
2017-12-24 00:02:00 +0530

    Update commit

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -6,7 +6,6 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
